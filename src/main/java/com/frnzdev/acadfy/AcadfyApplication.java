a03d8a0590e9d0f9ff6c50f1fdc0ee596e34b52d
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-02-16 20:27:31 -0300

    first commit

---
+++ src/main/java/com/frnzdev/acadfy/AcadfyApplication.java
@@ -0,0 +1,13 @@
+package com.frnzdev.acadfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frnzdev/acadfy/AcadfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frnzdev.acadfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
