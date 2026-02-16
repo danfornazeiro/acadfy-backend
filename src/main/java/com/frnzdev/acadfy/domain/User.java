d12f466ab9b100b9fdc6331d6fe89f5fe20903b3
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-02-16 20:28:30 -0300

    feat: adding security configs

---
+++ src/main/java/com/frnzdev/acadfy/domain/User.java
@@ -0,0 +1,33 @@
+package com.frnzdev.acadfy.domain;
+
+import com.frnzdev.acadfy.domain.enums.RoleUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String schoolName;
+
+    @Enumerated(EnumType.STRING)
+    private RoleUser roleUser;
+
+}
